aa1920e9abb0dfd3b3adb38736b5ef8cbcec1bab
Luiz Fernando <luiz-fernando@outlook.com>
2023-02-14 16:33:09 -0300

    Criado método de autenticação

--- core/views.py
+++ core/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
+# from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+def home(request):
+    return render(request, 'core/home.html')
+
+# @login_required
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        usuario = authenticate(request, username=username, password=password)
+        if usuario is not None:
+            return render(request, 'core/autenticacao.html')
+        return render(request, 'core/home.html')
+    
