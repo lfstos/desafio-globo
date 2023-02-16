7e9c837d05f82535e468372a7640f1822ea01909
Luiz Fernando <luiz-fernando@outlook.com>
2023-02-15 21:35:22 -0300

    Adiciona urls de gerenciamento e cadastro

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home),
     path('login', views.login),
+    path('gerenciamento', views.gerenciamento_usuarios),
+    path('cadastro', views.cadastro_usuario),
 ]
