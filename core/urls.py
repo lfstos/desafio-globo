fea92668f3dbce3d717a78d94ad0d28d9b68b3d3
Luiz Fernando <luiz-fernando@outlook.com>
2023-02-20 21:21:48 -0300

    Adiciona e configura urls

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+
 from . import views
 
+app_name = 'core'
+
 urlpatterns = [
-    path('', views.home),
-    path('login', views.login),
-    path('gerenciamento', views.gerenciamento_usuarios),
-    path('cadastro', views.cadastro_usuario),
+    path('', auth_views.LoginView.as_view(template_name='core/home.html'), name='home'),
+    path('principal/', views.principal, name='principal'),
+    path('gerenciamento/', views.gerenciamento_usuarios),
+    path('cadastro/', views.cadastro_usuario),
+    path('lista-usuarios/', views.lista_usuarios, name='lista-usuarios'),
+    path('cadastra-usuarios', views.cadastro_usuario_teste , name='cadastra-usuarios'),
+
+    # path('grafico-cpu/', views.gera_grafico_consumo_cpu),
+    # path('grafico-cluster/', views.gera_grafico_info_cluster),
 ]
