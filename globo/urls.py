af3eb90c97952937b1d9c70f7ce6bd5acd01cad0
Luiz Fernando <luiz-fernando@outlook.com>
2023-02-23 13:28:11 -0300

    Ajusta rotas do projeto

--- globo/urls.py
+++ globo/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LoginView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('django.contrib.auth.urls')),
+    path('', LoginView.as_view(), name='login'),
+    path('contas/', include('django.contrib.auth.urls')),
     path('core/', include('core.urls', namespace='core')),
 ]
